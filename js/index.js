ab61780d07834892f7d0498fac31c2269027c7f3
Vinicius <vinysales@hotmail.com>
2019-05-25 11:50:17 -0300

    site finalizado responsivo e com funcionalidades

--- js/index.js
+++ js/index.js
@@ -1,25 +1,85 @@
 $(document).ready(function () {
-    $("a").on('click', function (event) {
+    $(".desk a, .footer a").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
-                scrollTop: $(hash).offset().top-121
+                scrollTop: $(hash).offset().top - 121
             }, 800);
         }
     });
 });
 
-/***************************/
+$(document).ready(function () {
+    $("#modal-menu a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 50
+            }, 800);
+        }
+    });
+});
 
+/* scrollspy */
 
-  /* galeria */
+/*$(window).bind('scroll', function() {
+    var currentTop = $(window).scrollTop();
+    var elems = $('.scrollspy');
+    elems.each(function(index){
+      var elemTop 	= $(this).offset().top-122;
+      var elemBottom 	= elemTop + $(this).height();
+      if(currentTop >= elemTop && currentTop <= elemBottom){
+        var id 		= $(this).attr('id');
+        var navElem = $('a[href="#' + id+ '"]');
+    navElem.parent().addClass('active').siblings().removeClass( 'active' );
+      }
+    })
+});*/
 
-  $(document).ready(function() {
+$(window).bind('scroll', function () {
+    var currentTop = $(window).scrollTop();
+    var elems = $('section, main');
+    elems.each(function (index) {
+        var elemTop = $(this).offset().top-122;
+        var elemBottom = elemTop + $(this).height();
+        if (currentTop >= elemTop && currentTop <= elemBottom) {
+            var id = $(this).attr('id');
+            var navElem = $('a[href="#' + id + '"]');
+            navElem.parent().addClass('active').siblings().removeClass('active');
+        }
+    })
+});
+
+/* galeria */
+
+$(document).ready(function () {
     $('.portifolio-img').magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
         type: 'image',
-        gallery:{enabled:true}
+        gallery: { enabled: true }
         // other options
-      });
-  });
+    });
+});
+
+
+/* modal */
+
+var visibilidade = false;
+
+function ocultarExibir() {
+    if (visibilidade) {
+        document.getElementById("modal-menu").style.display = "none";
+        visibilidade = false;
+    } else {
+        document.getElementById("modal-menu").style.display = "block";
+        visibilidade = true;
+    }
+}
+
+function ocultar() {
+    document.getElementById("modal-menu").style.display = "none";
+    visibilidade = false;
+}
+
